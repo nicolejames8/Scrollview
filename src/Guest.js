796b6081e22b984e4e903ae890e7774eff8eb7dd
Nicole James <143070023+nicolejames8@users.noreply.github.com>
2023-11-11 22:26:12 -0800

    inital commit

---
+++ src/Guest.js
@@ -0,0 +1,82 @@
+import react from "react";
+
+export const guest = [
+    {
+        id: 1,
+        name: "OLAGUIR, MEMPHIS",
+        place: "Memphis, Tenessee"
+    },
+    {
+        id: 2,
+        name: "PAGUICAN, NICOLE JAMES",
+        place: "Japan, Tokyo"
+    },
+    {
+        id: 3,
+        name: "FELISILDA, ARNOLD JAMES",
+        place: "Switzerland"
+    },
+    {
+        id: 3,
+        name: "JANNAH, MARIE",
+        place: "Japan, kyoto"
+    },
+    {
+        id: 3,
+        name: "GARCIA, RODEL",
+        place: "Spain"
+    },
+    {
+        id: 3,
+        name: "JAGUNOS, JOANA",
+        place: "Philippines"
+    },
+    {
+        id: 3,
+        name: "PAGUICAN, KIM LEO",
+        place: "Canada"
+    },
+    {
+        id: 3,
+        name: "TAHIL, ADRIAN",
+        place: "Korea, Seoul"
+    },
+    {
+        id: 3,
+        name: "CARBONILLA, MARLON",
+        place: "Russia"
+    },
+    {
+        id: 3,
+        name: "JUNTILLA, HERSHIEL",
+        place: "United States of America"
+    },
+    {
+        id: 3,
+        name: "BUCIA, WARREN",
+        place: "India"
+    },
+    {
+        id: 3,
+        name: "PILIPINO, JESREL",
+        place: "Italy, Rome"
+    },
+    {
+        id: 3,
+        name: "MADANGUIT, RUSSEL",
+        place: "Cambodia"
+    },
+    {
+        id: 3,
+        name: "SARABIA, EARL MIKE",
+        place: "Japan, Hanopol"
+    },
+    {
+        id: 3,
+        name: "AMORA, KENT",
+        place: "Jupiter"
+    },
+
+]
+
+export default guest;
